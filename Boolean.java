a0938219242076d3c6b19e855299a1868ec043d4
devikagolla <62101515+devikagolla@users.noreply.github.com>
2020-03-18 12:23:52 +0530

    Add files via upload

---
+++ Boolean.java
@@ -0,0 +1,11 @@
+package pkgboolean;
+public class Boolean {
+    public static void main(String[] args) {
+        // TODO code application logic here 
+        boolean b=true;
+        boolean c=!b;
+        System.out.println(b+" "+c);
+        
+    }
+    
+}
