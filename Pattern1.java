74dd28ebddc1a9c421cab2f342c128a55714bf55
devikagolla <62101515+devikagolla@users.noreply.github.com>
2020-03-18 12:41:51 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,16 @@
+package pattern;
+public class Pattern {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                System.out.print("* ");
+            } 
+            System.out.println("");
+        } 
+        System.out.println("");
+    }
+    
+}
