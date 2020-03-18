1709fc4cdcae97d10d9c867be876c374ab55327b
devikagolla <62101515+devikagolla@users.noreply.github.com>
2020-03-18 12:24:57 +0530

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,13 @@
+
+package ifelse;
+public class IfElse {
+    public static void main(String[] args) {
+        // TODO code application logic here 
+        int a=3;
+        if(a%2==0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+    
+}
