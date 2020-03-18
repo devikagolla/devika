9d4d7df2cae21b21276a14b6b8202e723a5217b7
devikagolla <62101515+devikagolla@users.noreply.github.com>
2020-03-18 12:23:18 +0530

    Add files via upload

---
+++ Ascii.java
@@ -0,0 +1,12 @@
+
+package ascii;
+public class Ascii {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i=0;i<=127;i++)
+        {
+            System.out.printf("%d :%c\n",i,i);
+        }
+    }
+    
+}
