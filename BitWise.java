94dd203eab82f422540b6ec7b3f4a1e791fb3cf9
devikagolla <62101515+devikagolla@users.noreply.github.com>
2020-03-18 12:23:36 +0530

    Add files via upload

---
+++ BitWise.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bitwise;
+
+/**
+ *
+ * @author devika.golla
+ */
+public class BitWise {
+    public static void main(String[] args) {
+        // TODO code application logic here 
+        int a=5,b=10,c,d;
+        c=a&b;
+        d=a|b;
+        System.out.println(+c+" "+d);
+    }
+    
+}
