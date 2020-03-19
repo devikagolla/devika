922379b07a92e5f7685b186c45e3268714a4ecf1
devikagolla <62101515+devikagolla@users.noreply.github.com>
2020-03-19 18:48:20 +0530

    Add files via upload

---
+++ Vect.java
@@ -0,0 +1,18 @@
+package vect;
+
+import java.util.Vector;
+
+public class Vect {
+    public static void main(String[] args) {
+        
+        Vector<Integer> v=new Vector<Integer>();
+        v.add(5);
+        v.add(5);
+        v.add(5);
+        v.add(5);v.add(5);v.add(5);v.add(5);v.add(5);v.add(5);
+        for(int i:v)
+            System.out.println(i);
+        
+    }
+    
+}
